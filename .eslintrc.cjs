741ca88c58f607cd1ab500827b4584a71eefbda7
Umair Saleem <saleemakhtar35302@gmail.com>
2023-10-14 20:32:25 +0500

    add more eslint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,75 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:import/typescript',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'import',
+    '@typescript-eslint',
+    'prettier',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-unresolved': 'error',
+    // "@typescript-eslint/no-explicit-any": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/camelcase': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'warn',
+    camelcase: 'off',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: true,
+    },
+    react: {
+      version: 'detect',
+    },
   },
 };
